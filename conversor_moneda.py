42b80051bd8ae2b84da7da1d97ba361c4e644f83
oaleman-arm <oscar.aleman2018@gmail.com>
2022-01-28 20:55:58 -0600

    Fourth commit

--- conversor_moneda.py
+++ conversor_moneda.py
@@ -1,7 +1,7 @@
 
 tipo_cambio_usd = input("Ingrese el Tipo de Cambio del dia: C$")
 tipo_cambio_usd = float(tipo_cambio_usd)
-cordobas = input("Ingrese la cantidad total de dinero:")
+cordobas = input("Ingrese la cantidad total de dinero: ")
 cordobas = float(cordobas)
 opcion = input("Elija una operacion: \n 1- De cordobas a dolares \n 2- De Dolares a cordobas \n")
 opcion = int(opcion)
