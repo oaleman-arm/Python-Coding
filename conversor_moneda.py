607217e93526b971942b365e01e01f63bfb95422
oaleman-arm <oscar.aleman2018@gmail.com>
2022-01-28 20:53:30 -0600

    third commit

--- conversor_moneda.py
+++ conversor_moneda.py
@@ -1,7 +1,14 @@
+
 tipo_cambio_usd = input("Ingrese el Tipo de Cambio del dia: C$")
 tipo_cambio_usd = float(tipo_cambio_usd)
-cordobas = input("Ingrese la cantidad total de cordobas: C$ ")
+cordobas = input("Ingrese la cantidad total de dinero:")
 cordobas = float(cordobas)
-total_dolares = cordobas/tipo_cambio_usd
-total_dolares = str(total_dolares)
-print("El total en dolares es:$ " +total_dolares +"dolares")
+opcion = input("Elija una operacion: \n 1- De cordobas a dolares \n 2- De Dolares a cordobas \n")
+opcion = int(opcion)
+
+if opcion == 1: print("El total en dolares es:$ " + str(cordobas/tipo_cambio_usd))
+
+if opcion == 2: print("El total en dolares es:C$ " + str(cordobas*tipo_cambio_usd))
+
+if opcion > 2 and opcion < 1 : print("Elija una opcion valida")
+
